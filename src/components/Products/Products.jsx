030bb26993da512f56bb95ec9b2fe49e576d9a8d
mokshit108 <mokshit108@gmail.com>
2021-08-06 16:32:59 +0530

    Navbar Created

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import Product from './Product/Product';
-
+import useStyles from './styles';
 
 const products = [
-   { id:1, name:'Shoes', description:'Running shoes.',  price: '5' },
-   { id:2, name:'Macbook', description:'Apple macbook.',price: '10' },
+   { id:1, name:'Shoes', description:'Running shoes.',  price: '5', image: "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg" },
+   { id:2, name:'Macbook', description:'Apple macbook.',price: '10', image: "https://cdn.mos.cms.futurecdn.net/iYCQTPgBSdDmkYESfPkunh.jpg" },
 ];
 
 const Products = () => {
+  const classes = useStyles();
    return (
-    <main>
-    
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
-          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-              <Product product={product} />
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product}/>
           </Grid>
-
         ))}
       </Grid>
     </main>
