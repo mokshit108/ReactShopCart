6f4bf3eb124597c619110211521fbe05233c28a3
mokshit108 <mokshit108@gmail.com>
2021-08-07 20:47:07 +0530

    Add to Cart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { commerce } from './lib/commerce';
 import { Products, Navbar} from './components';
 const App = () => {
     const [products, setProducts] = useState([]);
-
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -13,18 +13,28 @@ const App = () => {
         setProducts(data);
       };
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+    };
+
+    const handleAddToCart = async (productId, quantity) => {
+      const item = await commerce.cart.add(productId, quantity);
+  
+      setCart(item.cart);
+    };
+
       useEffect(() => {
         fetchProducts();
-        
+        fetchCart();
       }, []);
 
     console.log(products);
-
+    console.log(cart);
       
     return (
         <div>
-           <Navbar/>
-           <Products products={products}  />
+           <Navbar totalItems={cart.total_items}/>
+           <Products products={products} onAddToCart={handleAddToCart}  />
 
         </div>
     )
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,7 +9,7 @@ import useStyles from './styles';
    { id:2, name:'Macbook', description:'Apple macbook.',price: '10', image: "https://cdn.mos.cms.futurecdn.net/iYCQTPgBSdDmkYESfPkunh.jpg" },
 ];*/
 
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
    return (
     <main className={classes.content}>
@@ -17,7 +17,7 @@ const Products = ({ products }) => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-            <Product product={product}/>
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
